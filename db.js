de98b0e26f6240d328eb0b1ea5418340d00067d1
Indujaes <144216311+Indujaes@users.noreply.github.com>
2026-02-17 13:30:40 +0530

    Fix db connection for Render deploy

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 // Connection String
 const db = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "empdb1",
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 // Test Connection 
